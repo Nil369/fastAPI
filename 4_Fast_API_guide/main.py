8fdb41de2b8a4667e96c4a8af01445087656e1cb
Akash_Halder(Nil) <halderakash872@gmail.com>
2025-01-29 21:10:23 +0530

    added path parameters & enum

--- 4_Fast_API_guide/main.py
+++ 4_Fast_API_guide/main.py
@@ -2,6 +2,38 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+# @app.get("/")
+
+# +++++++++++++++ path params +++++++++++++++++
+@app.get("/items/{item_id}")
+async def read_items(item_id:int):
+    return {"item_id":item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+# ++++++++++ Enum class +++++++++++++++++
+from enum import Enum
+
+class ModelName(str,Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name":model_name, "message":"Learning Deep Learning fr!"}
+
+    if model_name =="lenet":
+        return {"model_name":model_name,"message":"LeCNN for all images"}
+
+    return {"model_name":model_name,"message":"Other remaining models"}
